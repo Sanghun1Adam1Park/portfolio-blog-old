e29372982e9dc47a66b036dc44d32eba94a99ba9
Sanghun Adam Park <sanghunpark0628@gmail.com>
2025-04-16 14:06:39 +0200

    for backend rework

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.forms import ValidationError
-from django.db.models.functions import Lower
+from .fields import CaseInsensitiveCharField
 
 class Project(models.Model):
     """_summary_
@@ -14,60 +11,22 @@ class Project(models.Model):
     url = models.URLField()
     updated = models.DateTimeField(auto_now=False)
     visible = models.BooleanField(default=False) 
-    tags = models.ManyToManyField('Tag', blank=True)  # Many-to-many relationship with Tag
+    tags = models.ManyToManyField('Tag', blank=True) 
     
     def __str__(self):
         return f"{self.name} - {self.description}"
-    
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                Lower('name'), name='unique_lower_name'
-            )
-        ]
 
 class Category(models.Model):
     """Category model for grouping tags."""
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, unique=True)
-    has_main_key = models.BooleanField(default=False)  # To identify main category
-    color = models.CharField(max_length=7, default="#FFFFFF")  # Default color for the category
-    visible = models.BooleanField(default=False)  # To identify main category
+    category_name = CaseInsensitiveCharField(max_length=255, unique=True, primary_key=True)
+    color = models.CharField(max_length=7, default="#FFFFFF")  
     
     def __str__(self):
-        return f'#{self.name}'
+        return f'#{self.category_name}'
 
-class Tag(models.Model):
+class Tag(Category):
     """Tag model associated with a category."""
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, unique=True)  # globally unique
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='main_tag')
-    is_main_key = models.BooleanField(default=False)  # To identify main tag
-    color = models.CharField(max_length=7, blank=True)  # Inherit color from category
+    tag_name = CaseInsensitiveCharField(max_length=255, unique=True, primary_key=True)
 
-    def save(self, *args, **kwargs):
-        # Inherit color from the associated category
-        if not self.color and self.category:
-            self.color = self.category.color
-        super().save(*args, **kwargs)
-    
-    def clean(self):
-        # Check if another main tag already exists for the category
-        if self.is_main_key:
-            existing_main_tag = Tag.objects.filter(category=self.category, is_main_key=True).exclude(id=self.id).first()
-            if existing_main_tag:
-                raise ValidationError(f"A main tag already exists for the category '{self.category.name}'.")
-    
     def __str__(self):
-        return f'#{self.name}'
-
-@receiver(post_save, sender=Category)
-def create_default_tag(sender, instance, created, **kwargs):
-    """_summary_
-        Signal to create a default tag when a new category is created.
-        Also sets the category as having a main key.
-        """
-    if created:
-        Tag.objects.create(name=instance.name, category=instance, is_main_key=True)
-        instance.has_main_key = True
-        instance.save()
+        return f'# {super.__str__} - {self.tag_name}'
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from github import Github
 from .models import Project, Tag
 
+# Request handling functions (views)
 def about(request):
     """_summary_
         Returns HTML for 'about' page.
@@ -18,8 +19,8 @@ def projects(request):
     Args:
         request (_type_): django request from ./urls.py
     """
-    projects = Project.objects.all() # Get all projects from the database
-    tags = Tag.objects.all() # Get all tags from the database
+    projects = Project.objects.all() # Get all projects ojbects from the database used to display projects
+    tags = Tag.objects.all() # Get all tags ojbects from the database used in filter button
     return render(request, 'portfolio/projects.html', {'projects' : projects, 'tags': tags})
 
 def analytics(request):
@@ -29,8 +30,27 @@ def analytics(request):
     Args:
         request (_type_): django request from ./urls.py
     """
-    return render(request, 'portfolio/analytics.html', {})
+    return render(request, 'portfolio/analytics.html', {})                                         
 
+def filter(request, tag):
+    """_summary_
+        Returns HTML for 'filtered' page.
+        
+    Args:
+        request (_type_): django request from ./urls.py
+        tag (str): name tag to filter projects by
+    """
+    if tag.lower() == "all" :
+        return redirect('projects') # Redirect to all projects if tag is "all"
+    try:
+        tag = Tag.objects.get(name__iexact=tag) # Get the tag object from DB
+        projects = Project.objects.filter(tags=tag) # Get all projects that has the tag from DB
+        tags = Tag.objects.all() # Get all tags from DB for filter button 
+        return render(request, 'portfolio/filtered.html', {'projects' : projects, 'tag' : tag, 'tags': tags})
+    except Tag.DoesNotExist:
+        return redirect('projects') # Redirect to all projects if tag is not found
+
+# DB managing functions (views)
 def load_projects():
     """_summary_
         Updates the projects in the database by fetching from GitHub using GitHub REST API.   
@@ -40,30 +60,11 @@ def load_projects():
     my_repos = hub.get_user("sanghun1adam1park").get_repos()
     for repo in my_repos:
         Project.objects.update_or_create(
-            id=repo.id,
+            id=repo.id, # Check if repo is already in, if in update else create 
             defaults={
                 "name": repo.name,
                 "description": repo.description or "no description",
                 "url": repo.html_url,
                 "updated": repo.updated_at,
             }
-        )                                            
-
-def filter(request, tag):
-    """_summary_
-        Returns HTML for 'filtered' page.
-        
-    Args:
-        request (_type_): django request from ./urls.py
-        tag (_type_): tag to filter projects by
-    """
-    if tag.lower() == "all" :
-        return redirect('projects') # Redirect to all projects if tag is "all"
-    try:
-        tag = Tag.objects.get(name__iexact=tag) # Get the tag object from the database
-        projects = Project.objects.filter(tags__name__iexact=tag.name) # Get all projects from the database
-        print(projects)       
-        tags = Tag.objects.all() # Get all tags from the database
-        return render(request, 'portfolio/filtered.html', {'projects' : projects, 'tag' : tag, 'tags': tags})
-    except Tag.DoesNotExist:
-        return redirect('projects') # Redirect to all projects if tag is not found
+        )   
